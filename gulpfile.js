17289ed19a5ebb1e00be71f33dd505034248ad56
Kai Zau <k@kaizau.com>
2019-03-11 13:56:48 +0800

    WIP production build settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,21 @@ const { src, dest, series, parallel, watch } = require('gulp');
 const named = require('vinyl-named');
 const pathlib = require('path');
 const stylus = require('gulp-stylus');
+const autoprefixer = require('gulp-autoprefixer');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
-const webpack = require('webpack-stream');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 const pug = require('gulp-pug');
 const del = require('del');
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  require('now-env');
-}
+// ENV vars passed webpack and pug
+if (process.env.USE_LOCAL_ENV) require('now-env');
+
+const env = {
+  NODE_ENV: process.env.NODE_ENV || 'production',
+  EXAMPLE_BUILD_VAR: process.env.EXAMPLE_BUILD_VAR,
+};
 
 //
 // Static Site Compilation
@@ -19,11 +25,9 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 //
 // TODO
 // - data pipeline
-// - production
-// - consider rolling all compilation into webpack
 
 const assets = parallel(copied, css, js);
-const all = series(assets, markup);
+const all = series(assets, html);
 
 function copied() {
   return src(['site/**/*.{jpg,png,gif,svg}', '!site/_shared/**'])
@@ -32,31 +36,39 @@ function copied() {
 
 function css() {
   return src('site/**/index.styl')
-    .pipe(stylus({ include: 'site/' }))
+    .pipe(stylus({
+      include: 'site/',
+      'include css': true,
+      compress: env.NODE_ENV === 'production',
+    }))
+    .pipe(autoprefixer())
     .pipe(dest('dist/'));
 }
 
 function js() {
+  const mode = env.NODE_ENV === 'production' ? 'production' : 'development';
+  const filename = env.NODE_ENV === 'production' ? '[name].[chunkhash:8].js' : '[name].js';
   return src('site/**/index.js')
     .pipe(named(file => {
       const basename = file.dirname + '/' + file.stem;
       return pathlib.relative(file.base, basename);
     }))
     .pipe(eslint({ fix: true }))
-    // TODO Handle babel, process.env replacement, etc in webpack
     .pipe(babel())
-    .pipe(webpack({
-      mode: 'development',
-      output: {
-        filename: '[name].js',
-      },
+    .pipe(webpackStream({
+      mode,
+      output: { filename },
+      plugins: [ new webpack.EnvironmentPlugin(env) ]
     }))
     .pipe(dest('dist/'));
 }
 
-function markup() {
+function html() {
   return src(['site/**/*.pug', '!site/_shared/**'])
-    .pipe(pug({ basedir: 'site/' }))
+    .pipe(pug({
+      basedir: 'site/',
+      locals: env
+    }))
     .pipe(dest('dist/'));
 }
 
