144f9dca27f7174d9d1e0298847a3716f2867128
Kai Zau <k@kaizau.com>
2020-08-03 10:13:47 +0800

    Migrate to vercel

--- api/example.js
+++ api/example.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.send('OK: ' + process.env.EXAMPLE_API_VAR);
+app.get("*", (req, res) => {
+  res.json({ data: process.env.EXAMPLE_API_VAR });
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   // eslint-disable-next-line no-console
-  console.error('ERROR', err.status, err.message);
+  console.error("ERROR", err.status, err.message);
   res.sendStatus(err.status);
 });
 
