7bbf792da93b2f140032deb0447b8850e9a42f69
Kai Zau <kaizau@users.noreply.github.com>
2023-04-30 14:52:47 +0800

    Update dependencies, parcel, vercel

--- site/assets/core.js
+++ site/assets/core.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
 console.log("Hello, universe!");
-console.log("process.env.EXAMPLE_BUILD_VAR:", process.env.EXAMPLE_BUILD_VAR);
+
+console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
+
+console.log(
+  "process.env.EXAMPLE_VAR:",
+  process.env.EXAMPLE_VAR ? process.env.EXAMPLE_VAR : "(.env not found)"
+);
 
 fetch("/api/example")
   .then((res) => res.json())
   .then((json) => {
-    console.log("process.env.EXAMPLE_API_VAR:", json);
-  });
+    console.log("GET /api/example:", json);
+  })
+  .catch(() => console.log("GET /api/example: (api not found)"));
 /* eslint-enable no-console */
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./site/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
